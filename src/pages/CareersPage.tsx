d9ddecbdf5b449b17d1c92367d57e1323fc1cf1d
edenfinal <edenconstructionorsupplier@gmail.com>
2026-06-29 20:13:16 +0500

    Updated CareersPage.tsx

--- src/pages/CareersPage.tsx
+++ src/pages/CareersPage.tsx
@@ -59,7 +59,7 @@ function JobCard({ job, onApply, c }: { job: Job; onApply: () => void; c: (secti
             )}
             {job.salary_range && (
             <span className="text-sm text-gold-400 flex items-center gap-1">
-            <span className="font-semibold text-xs">PKR</span>
+            <span className="font-semibold">PKR</span>
             {job.salary_range}
             </span>
             )}
